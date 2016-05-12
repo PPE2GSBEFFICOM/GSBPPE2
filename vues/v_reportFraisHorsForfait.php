3a8d1a8bf90720b7fb38a596893d6697e38f814c
egrospiron <egrospiron@efficom-lille.com>
2016-05-12 17:53:37 +0200

    GSBfinal
    Ajout du site GSB

---
+++ vues/v_reportFraisHorsForfait.php
@@ -0,0 +1,6 @@
+﻿<div class="col-md-10">
+	<div class="input-group">
+			<input class="form-control" type="text" placeholder="Le frais a été reporté au mois suivant" disabled>
+				<span class="input-group-addon"><i class="fa fa-check"></i></span>
+	</div>
+</div>
